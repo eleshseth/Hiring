039413164ad07643fa5171fd628e2fc561be1c94
elesh seth <elesh.seth@gmail.com>
2025-06-12 23:35:39 +0530

    the chengs

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div className='logo'>Hiring</div>
+      <div className='logo'>ALTISLINK</div>
       <div className='navlinks'>
         <div>Home</div>
         <div>Menu</div>
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div>Contact Us</div>
         <div>Blogs</div>
       </div>
-      <div className='profile'>Profile</div>
+      <div className='profile'><button className='signupbutton'>signup</button></div>
     </div>
   );
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { assets } from '../../assets/assets';
+import './Home.css';
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <img src={assets.banner} />
+      <div className='banner-container'>
+        <img src={assets.banner} alt='banner' className='banner-image' />
+        <div className='banner-text'>
+          <h1>Empowering IT Excellence</h1>
+          <h2>Connecting Tech Talent with Tomorrow’s Success Stories</h2>
+          <button className='banner-button'>Get Started</button>
+        </div>
+      </div>
+      <div className='experience'>
+        <div className='experience-text'>45+ Years of Recruitment Experience</div>
+        <div></div>
       </div>
     </div>
   );
